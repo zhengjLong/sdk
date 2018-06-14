76f9b4822c63b42432adc2d233092b279339fc46
jerome_zheng <1577225141@qq.com>
2018-06-14 11:36:54 +0800

    add load round pic
    add load round pic

--- src/main/java/com/library/base/photopicker/utils/GlideDisplay.java
+++ src/main/java/com/library/base/photopicker/utils/GlideDisplay.java
@@ -17,8 +17,22 @@ import java.io.File;
 public class GlideDisplay {
 
 
-//    默认图片加载设置
+    /*由于是静态对象3个option需要独立开来*/
     private static RequestOptions options = new RequestOptions()
+            .centerCrop()
+            .placeholder(R.color.colorTextStyleA9)
+            .error(R.color.colorTextStyleA9)
+            .priority(Priority.HIGH)
+            .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+    private static RequestOptions optionsCircle = new RequestOptions()
+            .centerCrop()
+            .placeholder(R.color.colorTextStyleA9)
+            .error(R.color.colorTextStyleA9)
+            .priority(Priority.HIGH)
+            .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+    private static RequestOptions optionsRound = new RequestOptions()
             .centerCrop()
             .placeholder(R.color.colorTextStyleA9)
             .error(R.color.colorTextStyleA9)
@@ -27,7 +41,7 @@ public class GlideDisplay {
 
 
     /**
-     * 加载File
+     * 正常图片加载File
      * @param iv
      * @param file
      */
@@ -35,13 +49,56 @@ public class GlideDisplay {
         Glide.with(iv.getContext()).load(file).apply(options).into(iv);
     }
 
+
     /**
-     * 加载资源id路径
+     * 正常图片加载resId
      * @param iv
      * @param resId
      */
     public static void display(ImageView iv, int resId) {
-        Glide.with(iv.getContext()).load(resId)
+        Glide.with(iv.getContext()).load(resId).apply(options)
                 .into(iv);
     }
+
+
+    /**
+     * 正常图片加载bitmap
+     * @param iv
+     * @param bitmap
+     */
+    public static void display(ImageView iv, Bitmap bitmap) {
+        Glide.with(iv.getContext()).load(bitmap).apply(options)
+                .into(iv);
+    }
+
+
+    /**
+     * 加载图形图片
+     * @param iv
+     * @param url
+     */
+    public static void displayCircle(ImageView iv,String url) {
+        Glide.with(iv.getContext()).load(url).apply(getOptionsCircle())
+                .into(iv);
+    }
+
+    /**
+     * 加载图角图片
+     * @param iv
+     * @param url
+     * @param radius
+     */
+    public static void displayRound(ImageView iv,String url,int radius) {
+        Glide.with(iv.getContext()).load(url).apply(getOptionsRound(iv.getContext(),radius))
+                .into(iv);
+    }
+
+    private static RequestOptions getOptionsCircle (){
+        return  optionsCircle.circleCrop();
+    }
+
+    private static RequestOptions getOptionsRound (Context context,int radius){
+        return optionsRound.transform(new GlideRoundTransform(context,radius));
+    }
+
 }
